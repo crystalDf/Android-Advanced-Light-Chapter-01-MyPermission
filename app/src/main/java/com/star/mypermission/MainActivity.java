a4111960418680833ef527341c33cec021955bb0
crystalDf <chendong333@gmail.com>
2018-11-05 23:38:53 +0800

    MyPermission

--- app/src/main/java/com/star/mypermission/MainActivity.java
+++ app/src/main/java/com/star/mypermission/MainActivity.java
@@ -5,11 +5,12 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 public class MainActivity extends AppCompatActivity {
 
